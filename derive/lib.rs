c03d4d3ed5f0c70229d68eb3f48974f22798a814
Jakob Demler <jdemler@curry-software.com>
2017-01-24 19:42:43 +0100

    Make it compile with the current beta release branch

--- derive/lib.rs
+++ derive/lib.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![cfg_attr(not(test), feature(proc_macro, proc_macro_lib))]
-
 #[cfg(not(test))] extern crate proc_macro;
 #[macro_use] extern crate quote;
 extern crate syn;
